c880bff8a8e74863dd35cc0a0bc70b8da9591311
Blake Howell <sailpuff322@gmail.com>
2016-08-06 16:42:20 -0400

    Add Config for Multitenacy
    - Stub out config interface
    - Build config handler

---
+++ config/config.go
@@ -0,0 +1,16 @@
+package config
+
+var config Value
+var mu sync.Mutex
+
+func (config *Value) Load(){
+
+}
+
+func (config *Value) Upsert(read ){
+
+}
+
+func (config *Value) Get() map{
+
+}
--- main.go
+++ main.go
@@ -1,91 +1,114 @@
 package main
 
 import (
-        "net/http"
-        "log"
-        "encoding/json"
-        "strings"
-        "github.com/beh9540/slackpull/slack"
+	"encoding/json"
+	"github.com/beh9540/slackpull/slack"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type PullRequest struct {
-        IssueUrl string `json:"issue_url"`
-        Title string    `json:"title"`
-        Body string     `json:"body"`
-        Url string      `json:"url"`
-        HtmlUrl string  `json:"html_url"`
+	IssueUrl string `json:"issue_url"`
+	Title    string `json:"title"`
+	Body     string `json:"body"`
+	Url      string `json:"url"`
+	HtmlUrl  string `json:"html_url"`
 }
 
 type WebHook struct {
-        Action string                   `json:"action"`
-        PullRequest *PullRequest        `json:"pull_request"`
+	Action      string       `json:"action"`
+	PullRequest *PullRequest `json:"pull_request"`
 }
 
 type Label struct {
-        Url string
-        Name string
-        Color string
+	Url   string
+	Name  string
+	Color string
 }
 
 type Issue struct {
-         Labels []Label `json:"labels"`
- }
+	Labels []Label `json:"labels"`
+}
 
 func WebhookHandler(w http.ResponseWriter, r *http.Request) {
-        event := r.Header.Get("X-GitHub-Event")
-        log.Printf("Got event: %s", event)
-        var webHook WebHook
-        err := json.NewDecoder(r.Body).Decode(&webHook)
-        if err != nil {
-                log.Printf("Got error with process pullrequest json: %v", err)
-                w.WriteHeader(http.StatusInternalServerError)
-                return
-        }
-        switch event {
-        case "pull_request":
-                err = processPullRequest(&webHook)
-                if err != nil {
-                        log.Printf("Got error with getting issue: %v", err)
-                        w.WriteHeader(http.StatusInternalServerError)
-                        return
-                }
-        }
-        w.WriteHeader(http.StatusOK)
+	event := r.Header.Get("X-GitHub-Event")
+	log.Printf("Got event: %s", event)
+	var webHook WebHook
+	err := json.NewDecoder(r.Body).Decode(&webHook)
+	if err != nil {
+		log.Printf("Got error with process pullrequest json: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	switch event {
+	case "pull_request":
+		err = processPullRequest(&webHook)
+		if err != nil {
+			log.Printf("Got error with getting issue: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func ConfigHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		config, err := config.Upsert(r.Body)
+		if err != nil {
+			log.Printf("Got error upserting config: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Write(config)
+		w.WriteHeader(http.StatusOK)
+		break
+	case "GET":
+		config, err := config.Get()
+		w.Write(config)
+		w.WriteHeader(http.StatusOK)
+		break
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		break
+	}
 }
 
 func processPullRequest(webhook *WebHook) error {
-        var action string
-        action = webhook.Action
-        log.Printf("Got action: %s", action)
-        log.Printf("Got webhook: %s", webhook)
-        switch action {
-        case "labeled":
-                log.Printf("New Pull Request: %v", webhook.PullRequest)
-                pullRequest := webhook.PullRequest
-                resp, err := http.Get(pullRequest.IssueUrl)
-                if err != nil {
-                        return err
-                }
-                defer resp.Body.Close()
-                var issue Issue
-                if err = json.NewDecoder(resp.Body).Decode(&issue); err != nil {
-                        return err
-                }
-                labels := issue.Labels
-                log.Printf("Got labels: %v", labels)
-                for _, label := range labels {
-                        switch {
-                        case strings.Contains(label.Name, "ready for review"):
-                                return slack.NewReview(pullRequest.Title, pullRequest.Body, pullRequest.HtmlUrl)
-                        case strings.Contains(label.Name, "has been reviewed"):
-                                return slack.CompleteReview(pullRequest.Title)
-                        }
-                }
-        }
-        return nil
+	var action string
+	action = webhook.Action
+	log.Printf("Got action: %s", action)
+	log.Printf("Got webhook: %s", webhook)
+	switch action {
+	case "labeled":
+		log.Printf("New Pull Request: %v", webhook.PullRequest)
+		pullRequest := webhook.PullRequest
+		resp, err := http.Get(pullRequest.IssueUrl)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		var issue Issue
+		if err = json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+			return err
+		}
+		labels := issue.Labels
+		log.Printf("Got labels: %v", labels)
+		for _, label := range labels {
+			switch {
+			case strings.Contains(label.Name, "ready for review"):
+				return slack.NewReview(pullRequest.Title, pullRequest.Body, pullRequest.HtmlUrl)
+			case strings.Contains(label.Name, "has been reviewed"):
+				return slack.CompleteReview(pullRequest.Title)
+			}
+		}
+	}
+	return nil
 }
 
 func main() {
-        http.HandleFunc("/process", WebhookHandler)
-        log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/process", WebhookHandler)
+	http.HandleFunc("/config", ConfigHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
